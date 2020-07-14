e592e534efd65a4fcc7babdb2668fadea5ef1e05
bersK <stefan.karolev1@gmail.com>
2020-07-14 11:39:55 +0300

    guild and channel check vars

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,6 +1,10 @@
 const ping = require('./ping');
 const eightBall = require('./8ball');
 
+const guildID = process.env.GUILD_ID;
+const channelID = process.env.CHANNEL_ID;
+
+
 const commands = {
   ping,
   '8ball': eightBall,
@@ -13,7 +17,7 @@ module.exports = async (msg) => {
   // guild and channel check
   //   if (msg.guild.id === guildID && msg.channel.id === channelID) {
   const args = msg.content.split(' ');
-  if (args.length == 0 || args[0].charAt(0) !== '!') return;
+  if (args.length === 0 || args[0].charAt(0) !== '!') return;
   const command = args.shift().substr(1);
   if (Object.keys(commands).includes(command)) {
     commands[command](msg, args);
