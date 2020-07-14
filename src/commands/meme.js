96783aa5be6af9aed53ef19ce88919d55c6ca3a2
bersK <stefan.karolev1@gmail.com>
2020-07-14 22:40:42 +0300

    added object with sample reddit subs and a shitty help command

--- src/commands/meme.js
+++ src/commands/meme.js
@@ -1,12 +1,22 @@
 const imgapi = require('imageapi.js');
 
+const subOptions = {
+  1: 'meme',
+  2: 'dankmemes',
+  3: 'trashy',
+};
+
+// TODO: Some kind of sub validition to be an actual meme sub
 module.exports = async (msg, args) => {
-  if (args[0].length !== 0) {
-    const img = await imgapi(args[0], false);
+  if (Object.keys(subOptions).includes(args[0])) {
+    const img = await imgapi(subOptions[args[0]], false);
     await msg.channel.send(img);
-    console.log(`Subreddit: ${args[0]}`);
+
+    console.log(`Subreddit: ${subOptions[args[0]]}`);
+  } else if (args[0] === 'help' || args[0] === 'h') {
+    await msg.channel.send('Mess up harder next time.');
   } else {
-    const img = await imgapi('meme', false);
-    await msg.channel.send(img);
+    const response = `Invalid subreddit, the options are [number or name] \n${JSON.stringify(subOptions, null, 4)}`;
+    await msg.channel.send(response);
   }
 };
