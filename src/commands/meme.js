ffc7c2f616e8f52e03fee3f66441fea8a7e6fef3
bersK <stefan.karolev1@gmail.com>
2020-07-15 00:02:23 +0300

    Kinda broken

--- src/commands/meme.js
+++ src/commands/meme.js
@@ -8,15 +8,28 @@ const subOptions = {
 
 // TODO: Some kind of sub validition to be an actual meme sub
 module.exports = async (msg, args) => {
-  if (Object.keys(subOptions).includes(args[0])) {
-    const img = await imgapi(subOptions[args[0]], false);
-    await msg.channel.send(img);
+  if (Object.keys(subOptions).includes(args[0]) ||
+      Object.values(subOptions).includes(args[0]) 
+  ) {
+  // if (args[0].length !== 0) {
+    try {
+      await msg.channel.send(await imgapi(subOptions[args[0]], false));
+    } catch (error) {
+      console.log('Ooga booga fails');
+    }
 
     console.log(`Subreddit: ${subOptions[args[0]]}`);
   } else if (args[0] === 'help' || args[0] === 'h') {
-    await msg.channel.send('Mess up harder next time.');
+    try {
+      await msg.channel.send('Mess up harder next time.');
+    } catch (error) {
+      console.log('Error: help');
+    }
   } else {
-    const response = `Invalid subreddit, the options are [number or name] \n${JSON.stringify(subOptions, null, 4)}`;
+    const options = Object.values(subOptions).map((el) => `\n${el}`);
+    const response = `Invalid subreddit, the options are: ${options}`;
     await msg.channel.send(response);
+
+    console.log(`Argument 0:${args[0]}`);
   }
 };
